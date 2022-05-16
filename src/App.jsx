a906948888bcbb38be17ca05f339042787b0ec2a
Shunsuke-Gomisawa <105413803+Shunsuke-Gomisawa@users.noreply.github.com>
2022-05-16 13:47:40 +0900

    React_Practice_No.49

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  console.log("さいしょ");
+  //ここが状態を表す"state"である
+  const [num, setNum] = useState(0); //ここは初期値を入れる
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  //ここは"state"における関数処理を描く
+  const onClickUpUp = () => {
+    setNum(num + 1);
   };
+  //ボタンを押すと絵文字の表示非表示が切り替わる関数処理
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  //関心の分岐
+  useEffect(() => {
+    //3の倍数の時に顔文字が表示されるようにする
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslint-disable-next-line
+  }, [num]);
 
   //以下はJavaScriptの中でHTMLを描いている感じ
   return (
@@ -15,9 +38,18 @@ const App = () => {
       <ColorfulMessage color="pink">
         はい！サクラバクシンオーです！！
       </ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickUpUp}>カウントアップ!</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+
+      {faceShowFlag && <p>┌(┌^o^)┐</p>}
     </>
   );
 };
 
 export default App;
+
+//Reactは特定の条件の時にコンポーネントを再読み込みする　→
+//何か動作が行われる度に再レンダリングされるため、わざわざ再読み込みを行わなくても反映される
+//処理が重くなる原因にもなるのでのちのち注意！！
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ColorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   //オブジェクトの分割代入を使用("props.color→color"のように"props."の部分が省略できるようになる)
   const { color, children } = props;
   const contentStyle = {
@@ -11,5 +11,3 @@ const ColorfulMessage = (props) => {
   //例<p>"ここだ！！"</p>
   return <p style={contentStyle}>{children}</p>;
 };
-
-export default ColorfulMessage;
